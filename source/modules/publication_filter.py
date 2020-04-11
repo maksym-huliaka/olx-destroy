b2c1dcb62c2a4b72e504805d08184f53d1b60fe3
maksym-huliaka <60108106+maksym-huliaka@users.noreply.github.com>
2020-04-11 22:22:11 +0300

    Update publication_filter.py

--- source/modules/publication_filter.py
+++ source/modules/publication_filter.py
@@ -4,8 +4,8 @@ from datetime import datetime
 
 TIME_PATTERN = "([01]?[0-9]|2[0-3]):[0-5][0-9]"
 TODAY_PATTERN = "Сегодня"
-FILE_LAST_UPDATE_TIME_PATH = "resources/time.ini"
-FILE_WORDS_PATHS = "resources/words.ini"
+FILE_LAST_UPDATE_TIME_PATH = "/app/source/resources/time.ini"
+FILE_WORDS_PATHS = "/app/source/resources/words.ini"
 
 
 def filter_by_time(publications):
