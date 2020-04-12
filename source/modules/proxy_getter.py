14d7328b1f1b79d8d6830f55791f62ac4db969ca
maksym-huliaka <maxim.lviv1@gmail.com>
2020-04-12 18:44:52 +0300

    minor env changes

--- source/modules/proxy_getter.py
+++ source/modules/proxy_getter.py
@@ -7,6 +7,7 @@ import re
 def getDriver(proxy):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-extensions')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
