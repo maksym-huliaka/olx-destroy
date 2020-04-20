2ad75477a9d2d19138848e732096d84a31a820e7
maksym-huliaka <maxim.lviv1@gmail.com>
2020-04-20 17:14:42 +0300

    hotfix 2

--- source/modules/connection/connection_factory.py
+++ source/modules/connection/connection_factory.py
@@ -12,6 +12,7 @@ def getDriver(proxy):
     chrome_options.add_argument('--disable-extensions')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     if proxy:
         prefs = {'profile.default_content_setting_values': {'cookies': 2, 'images': 2, 'javascript': 2,
@@ -30,7 +31,6 @@ def getDriver(proxy):
         #chrome_options.add_argument('--proxy-server=socks5://' + proxy)
         chrome_options.add_argument('--proxy-server=%s' % proxy)
         chrome_options.add_argument("disable-infobars")
-        chrome_options.add_argument("--disable-extensions")
         chrome_options.add_experimental_option('prefs', prefs)
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
     #driver = webdriver.Chrome(options=chrome_options)
