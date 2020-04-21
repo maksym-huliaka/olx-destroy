2170706b47dbb63ba0a603799b27ab990d0bff59
maksym-huliaka <maxim.lviv1@gmail.com>
2020-04-22 01:57:24 +0300

    test2

--- source/modules/fingerprint/finger_test.py
+++ source/modules/fingerprint/finger_test.py
@@ -1,12 +1,15 @@
+import time
+
 from modules.connection.proxy_factory import get_driver
 
 
 def test_fingers():
     print("[WAIT][TEST] Checking for fingerprints..")
     driver = get_driver("")
-    driver.get('https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html')
+    driver.get('https://recaptcha-demo.appspot.com/recaptcha-v3-request-scores.php')
+    time.sleep(5)
     driver.save_screenshot("modules/fingerprint/screenshot4.png")
     html_source = driver.page_source
     print(html_source)
-    driver.close()
+    #driver.close()
     print("[OK][TEST] Fingerprints passed")
