8e200e85eb910a2e1162506e40b75daca4b41723
maksym-huliaka <maxim.lviv1@gmail.com>
2020-04-22 01:30:54 +0300

    test

--- source/modules/connection/connection_factory.py
+++ source/modules/connection/connection_factory.py
@@ -1,6 +1,6 @@
 import os
 
-#import chromedriver_binary
+import chromedriver_binary
 from gzip import compress, decompress
 
 from lxml import html
@@ -34,13 +34,13 @@ def get_driver(proxy):
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    #chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36")
     chrome_options.add_argument("window-size=1200,800")
     if proxy:
         prefs = {'profile.default_content_setting_values': {'cookies': 2, 'images': 2,
                                                             'plugins': 2, 'popups': 2, 'geolocation': 2,
-                                                            'notifications': 2, 'auto_select_certificate': 2,
+                                                            'auto_select_certificate': 2,
                                                             'mouselock': 2, 'mixed_script': 2, 'media_stream': 2,
                                                             'media_stream_mic': 2, 'media_stream_camera': 2,
                                                             'protocol_handlers': 2,
@@ -54,12 +54,12 @@ def get_driver(proxy):
         chrome_options.add_argument("disable-infobars")
         chrome_options.add_experimental_option('prefs', prefs)
 
-    driver = webdriver.Chrome(seleniumwire_options={'custom_response_handler': inject},
-                              executable_path=os.environ.get("CHROMEDRIVER_PATH"),
-                              options=chrome_options)
+    #driver = webdriver.Chrome(seleniumwire_options={'custom_response_handler': inject},
+    #                          executable_path=os.environ.get("CHROMEDRIVER_PATH"),
+    #                          options=chrome_options)
 
-    #driver = webdriver.Chrome(options=chrome_options,
-    #                         seleniumwire_options={'custom_response_handler': inject})
+    driver = webdriver.Chrome(options=chrome_options,
+                             seleniumwire_options={'custom_response_handler': inject})
 
     driver.header_overrides = {'Accept-Encoding': 'gzip'}
     return driver
--- source/modules/fingerprint/finger_test.py
+++ source/modules/fingerprint/finger_test.py
@@ -5,7 +5,7 @@ def test_fingers():
     print("[WAIT][TEST] Checking for fingerprints..")
     driver = get_driver("")
     driver.get('https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html')
-    #driver.save_screenshot("modules/fingerprint/screenshot4.png")
+    driver.save_screenshot("modules/fingerprint/screenshot4.png")
     html_source = driver.page_source
     print(html_source)
     driver.close()
