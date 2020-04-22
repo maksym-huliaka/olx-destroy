8f6672abb481d82547bd6affcffc7e83aa3363e8
maksym-huliaka <maxim.lviv1@gmail.com>
2020-04-23 02:40:15 +0300

    f

--- source/modules/path.py
+++ source/modules/path.py
@@ -1,5 +1,5 @@
-# FILE_APP_PROPERTIES = '/app/source/resources/app.properties'
-# FILE_JS_INJECTOR ='/app/source/modules/fingerprint/js/injector.js'
+FILE_APP_PROPERTIES = '/app/source/resources/app.properties'
+FILE_JS_INJECTOR ='/app/source/modules/fingerprint/js/injector.js'
 
-FILE_APP_PROPERTIES='resources/app.properties'
-FILE_JS_INJECTOR ='modules/fingerprint/js/injector.js'
+# FILE_APP_PROPERTIES='resources/app.properties'
+# FILE_JS_INJECTOR ='modules/fingerprint/js/injector.js'
