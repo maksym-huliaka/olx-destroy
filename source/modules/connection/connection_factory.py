2368164cbd0e455f34816198bb529d67cd1bba90
maksym-huliaka <maxim.lviv1@gmail.com>
2020-04-22 13:49:34 +0300

    linux

--- source/modules/connection/connection_factory.py
+++ source/modules/connection/connection_factory.py
@@ -34,7 +34,7 @@ def get_driver(proxy):
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    #chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36")
     chrome_options.add_argument("window-size=1200,800")
     if proxy:
@@ -55,7 +55,7 @@ def get_driver(proxy):
         chrome_options.add_experimental_option('prefs', prefs)
 
     driver = webdriver.Chrome(seleniumwire_options={'custom_response_handler': inject},
-                             executable_path=os.environ.get("CHROMEDRIVER_PATH"),
+                             #executable_path=os.environ.get("CHROMEDRIVER_PATH"),
                              options=chrome_options)
     # chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
     # driver = webdriver.Chrome(options=chrome_options,
