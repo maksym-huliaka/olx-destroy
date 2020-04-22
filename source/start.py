531d2e4b99cc140febf0ab2f0f649afe763307ce
maksym-huliaka <maxim.lviv1@gmail.com>
2020-04-22 20:38:06 +0300

    changes

--- source/start.py
+++ source/start.py
@@ -3,7 +3,6 @@ from modules.fingerprint.proxy_test import check_driver_2ip
 from modules.scheduler import start_scheduler
 from modules.telegram.bot import BOT
 
-test_fingers()
 print("[OK] Program started")
 start_scheduler()
 BOT.polling()
