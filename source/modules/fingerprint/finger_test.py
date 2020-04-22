a3b483f6d99f0110f1491ca68a2e2fdcf88c72e5
maksym-huliaka <maxim.lviv1@gmail.com>
2020-04-22 14:15:15 +0300

    test

--- source/modules/fingerprint/finger_test.py
+++ source/modules/fingerprint/finger_test.py
@@ -8,8 +8,8 @@ def test_fingers():
     driver = get_driver("")
     driver.get('https://recaptcha-demo.appspot.com/recaptcha-v3-request-scores.php')
     time.sleep(5)
-    driver.save_screenshot("modules/fingerprint/screenshot4.png")
+    #driver.save_screenshot("modules/fingerprint/screenshot4.png")
     html_source = driver.page_source
     print(html_source)
-    #driver.close()
+    driver.close()
     print("[OK][TEST] Fingerprints passed")
