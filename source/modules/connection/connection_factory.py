d90855fa3ee059e09a8d659ddef5f863b8b18097
maksym-huliaka <maxim.lviv1@gmail.com>
2020-04-23 02:58:12 +0300

    f

--- source/modules/connection/connection_factory.py
+++ source/modules/connection/connection_factory.py
@@ -30,31 +30,13 @@ def inject(req, req_body, res, res_body):
 def get_driver(proxy):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--disable-extensions')
     chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36")
     chrome_options.add_argument("window-size=1200,800")
     wire_options = None
     if proxy:
-        prefs = {'profile.default_content_setting_values': {'cookies': 2, 'images': 2,
-                                                            'plugins': 2, 'popups': 2, 'geolocation': 2,
-                                                            'auto_select_certificate': 2,
-                                                            'mouselock': 2, 'mixed_script': 2, 'media_stream': 2,
-                                                            'media_stream_mic': 2, 'media_stream_camera': 2,
-                                                            'protocol_handlers': 2,
-                                                            'ppapi_broker': 2, 'automatic_downloads': 2,
-                                                            'midi_sysex': 2,
-                                                            'push_messaging': 2, 'ssl_cert_decisions': 2,
-                                                            'metro_switch_to_desktop': 2,
-                                                            'protected_media_identifier': 2, 'app_banner': 2,
-                                                            'site_engagement': 2,
-                                                            'durable_storage': 2}}
-        chrome_options.add_argument("disable-infobars")
-        chrome_options.add_experimental_option('prefs', prefs)
         wire_options = {
             'custom_response_handler': inject,
             'proxy': {
