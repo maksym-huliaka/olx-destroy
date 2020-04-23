b9c35ee9fddf72a588d4c29917b23b87716f9d5f
maksym-huliaka <maxim.lviv1@gmail.com>
2020-04-23 03:01:32 +0300

    f

--- source/modules/connection/connection_factory.py
+++ source/modules/connection/connection_factory.py
@@ -31,6 +31,7 @@ def get_driver(proxy):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36")
