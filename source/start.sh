9388968b9a292f45815d688a529eb9c810d31bc0
maksym-huliaka <maxim.lviv1@gmail.com>
2020-04-24 21:06:00 +0300

    d

--- source/start.sh
+++ source/start.sh
@@ -2,7 +2,7 @@
 
 (
     while true; do
-        python3 main.py >> output.log
+        python3 main.py > output.log
         echo "[FATAL] crashed with exit code $?.  Respawning... " >&2
         sleep 5
     done
