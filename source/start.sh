1e5eca71d4286cf9a41f9a0a91487c63841dd220
maksym-huliaka <maxim.lviv1@gmail.com>
2020-04-24 19:56:23 +0300

    dw

--- source/start.sh
+++ source/start.sh
@@ -2,7 +2,7 @@
 
 (
     while true; do
-        python3 main.py > output.log &
+        python3 main.py
         echo "MyApp crashed with exit code $?.  Respawning... " >&2
         sleep 5
     done
